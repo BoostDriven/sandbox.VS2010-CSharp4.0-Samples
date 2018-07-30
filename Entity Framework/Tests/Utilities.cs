a982cdf1cfffc6d4f86d382b14b6f4375cbb1af7
tptaylor <troytaylor.net@gmail.com>
2018-07-30 10:11:59 -0400

    init

---
+++ Entity Framework/Tests/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Utilities
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+
+
+
